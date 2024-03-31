9495bf4bbe0e590e3935ddce753330f33c3bb9d9
Freddy Okino <freddyokino@hotmail.com>
2024-03-31 18:43:22 -0400

    criado as regras para criar questoes e buscar o numero da ultima, foi feita as rotas de cada regra tambem

--- src/routes/QuestaoRoute.js
+++ src/routes/QuestaoRoute.js
@@ -4,10 +4,10 @@ const questaoService = require('./../services/QuestaoService');
 
 // Middleware para fazer a conversão do campo numero para número
 router.use((req, res, next) => {
-  if (req.body.numero && typeof req.body.numero === 'string') {
-    req.body.numero = parseInt(req.body.numero);
-  }
-  next();
+    if (req.body.numero && typeof req.body.numero === 'string') {
+        req.body.numero = parseInt(req.body.numero);
+    }
+    next();
 });
 /*
 router.post('', async (req, res) => {
@@ -23,6 +23,80 @@ router.post('', async (req, res) => {
         res.status(500).json({ message: error.message });
     }
 });*/
+
+//rotas para cirar as questoes de acordo com o banco de dados de cada uma
+router.post('/artes', async (req, res) => {
+    try {
+        const novaQuestao = await questaoService.criarQuestaoArtes(req.body);
+        if (novaQuestao) {
+            res.json(novaQuestao);
+        } else {
+            res.status(500).json({ message: 'Erro ao criar a questão. Por favor, tente novamente mais tarde.' });
+        }
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
+router.post('/biologia', async (req, res) => {
+    try {
+        const novaQuestao = await questaoService.criarQuestaoBio(req.body);
+        if (novaQuestao) {
+            res.json(novaQuestao);
+        } else {
+            res.status(500).json({ message: 'Erro ao criar a questão. Por favor, tente novamente mais tarde.' });
+        }
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
+router.post('/filosofia', async (req, res) => {
+    try {
+        const novaQuestao = await questaoService.criarQuestaoFilosofia(req.body);
+        if (novaQuestao) {
+            res.json(novaQuestao);
+        } else {
+            res.status(500).json({ message: 'Erro ao criar a questão. Por favor, tente novamente mais tarde.' });
+        }
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
+router.post('/fisica', async (req, res) => {
+    try {
+        const novaQuestao = await questaoService.criarQuestaoFisica(req.body);
+        if (novaQuestao) {
+            res.json(novaQuestao);
+        } else {
+            res.status(500).json({ message: 'Erro ao criar a questão. Por favor, tente novamente mais tarde.' });
+        }
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
+router.post('/geografia', async (req, res) => {
+    try {
+        const novaQuestao = await questaoService.criarQuestaoGeografia(req.body);
+        if (novaQuestao) {
+            res.json(novaQuestao);
+        } else {
+            res.status(500).json({ message: 'Erro ao criar a questão. Por favor, tente novamente mais tarde.' });
+        }
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
+router.post('/historia', async (req, res) => {
+    try {
+        const novaQuestao = await questaoService.criarQuestaoHistoria(req.body);
+        if (novaQuestao) {
+            res.json(novaQuestao);
+        } else {
+            res.status(500).json({ message: 'Erro ao criar a questão. Por favor, tente novamente mais tarde.' });
+        }
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
 router.post('/matematica', async (req, res) => {
     try {
         const novaQuestao = await questaoService.criarQuestaoMath(req.body);
@@ -32,7 +106,6 @@ router.post('/matematica', async (req, res) => {
             res.status(500).json({ message: 'Erro ao criar a questão. Por favor, tente novamente mais tarde.' });
         }
     } catch (error) {
-        console.error('Erro ao criar questão:', error);
         res.status(500).json({ message: error.message });
     }
 });
@@ -45,32 +118,80 @@ router.post('/quimica', async (req, res) => {
             res.status(500).json({ message: 'Erro ao criar a questão. Por favor, tente novamente mais tarde.' });
         }
     } catch (error) {
-        console.error('Erro ao criar questão:', error);
         res.status(500).json({ message: error.message });
     }
 });
-router.get('',async (req,res)=>{
+router.get('', async (req, res) => {
     try {
         const achou = await questaoService.buscarQuestoes();
         res.json(achou)
     } catch (error) {
-        
+
     }
 })
-router.get("/ultimoMath", async(req,res)=>{
+//rotas para buscar ultima questao feita
+router.get("/ultimoArtes", async (req, res) => {
+    try {
+        const achou = await questaoService.buscarUltimoNumeroArtes();
+        res.json(achou)
+    } catch (error) {
+
+    }
+});
+router.get("/ultimoBiologia", async (req, res) => {
+    try {
+        const achou = await questaoService.buscarUltimoNumeroBio();
+        res.json(achou)
+    } catch (error) {
+
+    }
+});
+router.get("/ultimoFilosofia", async (req, res) => {
+    try {
+        const achou = await questaoService.buscarUltimoNumeroFilosofia();
+        res.json(achou)
+    } catch (error) {
+
+    }
+});
+router.get("/ultimoFisica", async (req, res) => {
+    try {
+        const achou = await questaoService.buscarUltimoNumeroFisica();
+        res.json(achou)
+    } catch (error) {
+
+    }
+});
+router.get("/ultimoGeografia", async (req, res) => {
+    try {
+        const achou = await questaoService.buscarUltimoNumeroGeografia();
+        res.json(achou)
+    } catch (error) {
+
+    }
+});
+router.get("/ultimoHistoria", async (req, res) => {
+    try {
+        const achou = await questaoService.buscarUltimoNumeroHistoria();
+        res.json(achou)
+    } catch (error) {
+
+    }
+});
+router.get("/ultimoMath", async (req, res) => {
     try {
         const achou = await questaoService.buscarUltimoNumeroMath();
         res.json(achou)
     } catch (error) {
-        
+
     }
 });
-router.get("/ultimoQuimica", async(req,res)=>{
+router.get("/ultimoQuimica", async (req, res) => {
     try {
         const achou = await questaoService.buscarUltimoNumeroQuimica();
         res.json(achou)
     } catch (error) {
-        
+
     }
-}) 
+})
 module.exports = router;
--- src/services/QuestaoService.js
+++ src/services/QuestaoService.js
@@ -1,8 +1,15 @@
 const questionModel = require('./../models/Questoes');
+const questaoArtes = require("./../models/QuestoesArtes");
+const questaoBiologia = require("../models/QuestoesBiologia");
+const questaoFilosofia = require("../models/QuestoesFilosofia");
+const questaoFisica = require("../models/QuestoesFisica");
+const questaoGeografia = require("../models/QuestoesGeografia");
+const questaoHistoria = require("../models/QuestoesHistoria");
 const questaoMath = require("./../models/QuestoesMath");
-const questaoQuimica=require("./../models/QuestoesQuimica")
+const questaoQuimica = require("./../models/QuestoesQuimica")
 
 module.exports = {
+    /*
     criarQuestao: async (questao) => {
         try {
             let novaQuestao = await questionModel.create(questao);
@@ -11,13 +18,61 @@ module.exports = {
             console.error('Erro ao criar questão:', error);
             throw new Error('Erro ao criar a questão. Por favor, tente novamente mais tarde.');
         }
+    },*/
+    //regras para criar questões
+    criarQuestaoArtes: async (questao) => {
+        try {
+            let novaQuestao = await questaoArtes.create(questao);
+            return novaQuestao
+        } catch (error) {
+            throw new Error('Erro ao criar a questão. Por favor, tente novamente mais tarde.');
+        }
+    },
+    criarQuestaoBio: async (questao) => {
+        try {
+            let novaQuestao = await questaoBiologia.create(questao);
+            return novaQuestao
+        } catch (error) {
+            throw new Error('Erro ao criar a questão. Por favor, tente novamente mais tarde.');
+        }
+    },
+    criarQuestaoFilosofia: async (questao) => {
+        try {
+            let novaQuestao = await questaoFilosofia.create(questao);
+            return novaQuestao
+        } catch (error) {
+            throw new Error('Erro ao criar a questão. Por favor, tente novamente mais tarde.');
+        }
+    },
+    criarQuestaoFisica: async (questao) => {
+        try {
+            let novaQuestao = await questaoFisica.create(questao);
+            return novaQuestao
+        } catch (error) {
+            throw new Error('Erro ao criar a questão. Por favor, tente novamente mais tarde.');
+        }
+    },
+    criarQuestaoGeografia: async (questao) => {
+        try {
+            let novaQuestao = await questaoGeografia.create(questao);
+            return novaQuestao
+        } catch (error) {
+            throw new Error('Erro ao criar a questão. Por favor, tente novamente mais tarde.');
+        }
+    },
+    criarQuestaoHistoria: async (questao) => {
+        try {
+            let novaQuestao = await questaoHistoria.create(questao);
+            return novaQuestao
+        } catch (error) {
+            throw new Error('Erro ao criar a questão. Por favor, tente novamente mais tarde.');
+        }
     },
     criarQuestaoMath: async (questao) => {
         try {
             let novaQuestao = await questaoMath.create(questao);
             return novaQuestao;
         } catch (error) {
-            console.error('Erro ao criar questão:', error);
             throw new Error('Erro ao criar a questão. Por favor, tente novamente mais tarde.');
         }
     },
@@ -26,28 +81,64 @@ module.exports = {
             let novaQuestao = await questaoQuimica.create(questao);
             return novaQuestao;
         } catch (error) {
-            console.error('Erro ao criar questão:', error);
             throw new Error('Erro ao criar a questão. Por favor, tente novamente mais tarde.');
         }
     },
-    buscarQuestoes: async() =>{
+    buscarQuestoes: async () => {
         try {
             let todas = await questionModel.find();
             return todas
         } catch (error) {
-            
+
         }
     },
-    buscarUltimoNumeroMath: async()=>{
-        
-            let numero = await questaoMath.findOne().sort({numero:-1})
-            return numero
-        
-    },
-    buscarUltimoNumeroQuimica: async()=>{
-        
-        let numero = await questaoQuimica.findOne().sort({numero:-1})
+    // regras para buscar o ultimo numero para usar ao criar a questao
+    buscarUltimoNumeroArtes: async () => {
+
+        let numero = await questaoArtes.findOne().sort({ numero: -1 })
         return numero
-    
-}
+
+    },
+    buscarUltimoNumeroBio: async () => {
+
+        let numero = await questaoBiologia.findOne().sort({ numero: -1 })
+        return numero
+
+    },
+    buscarUltimoNumeroFilosofia: async () => {
+
+        let numero = await questaoFilosofia.findOne().sort({ numero: -1 })
+        return numero
+
+    },
+    buscarUltimoNumeroFisica: async () => {
+
+        let numero = await questaoFisica.findOne().sort({ numero: -1 })
+        return numero
+
+    },
+    buscarUltimoNumeroGeografia: async () => {
+
+        let numero = await questaoGeografia.findOne().sort({ numero: -1 })
+        return numero
+
+    },
+    buscarUltimoNumeroHistoria: async () => {
+
+        let numero = await questaoHistoria.findOne().sort({ numero: -1 })
+        return numero
+
+    },
+    buscarUltimoNumeroMath: async () => {
+
+        let numero = await questaoMath.findOne().sort({ numero: -1 })
+        return numero
+
+    },
+    buscarUltimoNumeroQuimica: async () => {
+
+        let numero = await questaoQuimica.findOne().sort({ numero: -1 })
+        return numero
+
+    }
 };
