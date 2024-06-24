a661acdd775115e28607c17aee6adb8116baad5e
FreddyOkino <freddyokino@hotmail.com>
2024-06-24 19:21:19 -0400

    fazendo a rota para pegar 5 os melhores

--- src/routes/perfilRoute.js
+++ src/routes/perfilRoute.js
@@ -3,13 +3,22 @@ const router = express.Router()
 const perfilService = require('./../services/perfilService')
 
 
+router.get("/melhores", async (req, res) => {
+    try {
+        const resposta = await perfilService.buscarmelhores();
+        res.json(resposta)
+    } catch (error) {
+        res.status(500).json({ message: error.message })
+
+    }
+})
 router.post('', async (req, res) => {
     try {
         const resposta = await perfilService.cadastrar(req.body)
         res.json(resposta)
 
     } catch (error) {
-        throw { message: message.error, status: 500 }
+        res.status(500).json({ message: error.message })
     }
 })
 router.post("/pontuacao", async (req, res) => {
@@ -17,7 +26,7 @@ router.post("/pontuacao", async (req, res) => {
         const resposta = await perfilService.atualizarPontuacao(req.body)
         res.json(resposta)
     } catch (error) {
-
+        res.status(500).json({ message: error.message })
     }
 })
 router.get("/:id", async (req, res) => {
@@ -25,7 +34,10 @@ router.get("/:id", async (req, res) => {
         const resposta = await perfilService.buscarPorId(req.params.id)
         res.json(resposta)
     } catch (error) {
-
+        res.status(500).json({ message: error.message })
     }
 })
+
+
+
 module.exports = router
